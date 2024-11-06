3d439544cd549339320998cbea8cc7e860ca2324
JulianCooking <2601julian@gmail.com>
2024-11-06 22:29:55 +0100

    k

--- Stad in 2050/Assets/Scripts/deurtje.cs
+++ Stad in 2050/Assets/Scripts/deurtje.cs
@@ -1,8 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class Deur : MonoBehaviour
+
+public class deurtje : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
