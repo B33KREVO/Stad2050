345f4043e789ab627dfe2d817b76a13734e2b61b
JulianCooking <2601julian@gmail.com>
2024-11-06 20:00:56 +0100

    f

--- Stad in 2050/Assets/Scripts/CMD-First-Person-Controller-v1-1/PlayerLook.cs
+++ Stad in 2050/Assets/Scripts/CMD-First-Person-Controller-v1-1/PlayerLook.cs
@@ -22,7 +22,7 @@ public class PlayerLook : MonoBehaviour
       Cursor.lockState = CursorLockMode.Locked;
       rotation.y += Input.GetAxis("Mouse X");
       rotation.x += -Input.GetAxis("Mouse Y");
-      rotation.x = Mathf.Clamp(rotation.x, -15f, 15f);
+      rotation.x = Mathf.Clamp(rotation.x, -30f, 30f);
       transform.eulerAngles = new Vector2(0,rotation.y) * lookSpeed;
       Camera.main.transform.localRotation = Quaternion.Euler(rotation.x * lookSpeed, 0, 0);
     }
