7fd2137cbefa7fea5b443dc0c2a58df354f6db71
JulianCooking <2601julian@gmail.com>
2024-11-13 17:06:19 +0100

    push binnen
    fa

---
+++ Stad in 2050/Assets/voetstappenbuiten.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class voetstappenbinnen : MonoBehaviour
+{
+    public AudioSource voetstapAudio; // Voeg een AudioSource toe
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Zorg ervoor dat de AudioSource is ingesteld op het object
+        if (voetstapAudio == null)
+        {
+            voetstapAudio = GetComponent<AudioSource>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Controleer of W, A of D is ingedrukt en of de audio niet al speelt
+        if ((Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)) && !voetstapAudio.isPlaying)
+        {
+            voetstapAudio.Play(); // Speel de voetstapgeluid af
+        }
+        // Stop de audio als geen van de toetsen meer wordt ingedrukt
+        else if (!Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
+        {
+            voetstapAudio.Stop();
+        }
+    }
+}
