b96e0d717b95512dbf54cee3de56ccd5174768e4
JulianCooking <2601julian@gmail.com>
2024-11-13 20:49:20 +0100

    buurman gepusht

--- Stad in 2050/Assets/buurman.cs
+++ Stad in 2050/Assets/buurman.cs
@@ -8,16 +8,32 @@ public class buurman : MonoBehaviour
 {
     public string missietekst;  // Deze tekst willen we veranderen
     public TextMeshProUGUI missionText;
-    public GameObject missionBox;
+    public GameObject Buurman;
+    public string buurmantekst;  // Deze tekst willen we veranderen
+    public TextMeshProUGUI burmantext;
 
     void Start()
     {
-        missionBox.SetActive(true);
+        Buurman.SetActive(false);
     }
 
     // Update is called once per frame
     public void praten() {
         missietekst = "Go home to watch the news!";
         missionText.text = missietekst;  // Verander de tekst van de UI
+        buurmantekst = "Have you heard? It is all over the news, we are about to be flooded!";
+        burmantext.text = buurmantekst;  // Verander de tekst van de UI
+        Buurman.SetActive(true);
+        StartCoroutine(WachtEnVerwijder());
     }
+        IEnumerator WachtEnVerwijder()
+    {
+        // Wacht 5 seconden
+        yield return new WaitForSeconds(5f);
+
+        // Verwijder het object (zelfs als het dit script is)
+        Buurman.SetActive(false);
+        buurmantekst = " ";
+        burmantext.text = buurmantekst;  // Verander de tekst van de UI
+}
 }
