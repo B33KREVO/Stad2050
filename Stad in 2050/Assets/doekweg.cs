8f002f241c965e527c227e7fad3e5bd0841fe67b
JulianCooking <2601julian@gmail.com>
2024-11-13 14:33:07 +0100

    boot terrain 2

---
+++ Stad in 2050/Assets/doekweg.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doekweg : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void doekGone() {
+        gameObject.SetActive(false);
+    }
+}
