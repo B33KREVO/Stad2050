13c09018864860e49b550b91b0b46c2b4df5b198
JulianCooking <2601julian@gmail.com>
2024-11-13 12:14:25 +0100

    scene boven

--- Stad in 2050/Assets/Scripts/Boven/bovenscene.cs
+++ Stad in 2050/Assets/Scripts/Boven/bovenscene.cs
@@ -1,17 +1,11 @@
 using System.Collections;
-using System.Collections.Generic;
+using System.Collections.Genereic;
 using UnityEngine;
-using UnityEngine.seceneManagement;
+using UnityEngine.SceneManagement;
 
 public class bovenscene : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+{   
     void OnTriggerEnter() {
-        SceneManager.loadscene("boven2", LoadSceneMode.Single);
+        SceneManager.Loadscene("boven2", LoadSceneMode.Single);
     }
 }
